bf253f1a9e9b16b279168492e3abb8fbd0759f70
soundestmammal <robert.checco@gmail.com>
2018-01-30 16:16:34 -0500

    Model for Predicting house price - sklearn

--- housing/start.py
+++ housing/start.py
@@ -1,1 +1,10 @@
+# Importing the Libraries
+import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
+
+melbourne_file_path = "melb_data.csv"
+melbourne_data = pd.read_csv(melbourne_file_path)
+melbourne_price_data = melbourne_data.Price
+
+print(melbourne_price_data.head())
