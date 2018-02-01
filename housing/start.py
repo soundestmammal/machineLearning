ff27cd48881e25fd856f21b8ca69d62b197a670b
soundestmammal <robert.checco@gmail.com>
2018-01-31 23:27:18 -0500

    Began to work in Multiple Regression

--- housing/start.py
+++ housing/start.py
@@ -8,3 +8,25 @@ melbourne_data = pd.read_csv(melbourne_file_path)
 melbourne_price_data = melbourne_data.Price
 
 print(melbourne_price_data.head())
+
+columns_of_interest = ['Landsize' , 'BuildingArea']
+two_columns_of_data = melbourne_data[columns_of_interest]
+two_columns_of_data.describe()
+
+melbourne_data = melbourne_data.dropna() 
+
+y = melbourne_data.
+melbourne_predictors = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea', 
+                        'YearBuilt', 'Lattitude', 'Longtitude']
+
+X = melbourne_data[melbourne_predictors]
+
+from sklearn.tree import DecisionTreeRegressor
+
+melbourne_model = DecisionTreeRegressor()
+melbourne_model.fit(X , y)
+
+print("Making predictions for the following 5 houses:")
+print(X.head())
+print("The predictions are")
+print(melbourne_model.predict(X.head()
