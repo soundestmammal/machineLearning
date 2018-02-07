f022e7893d927188c9b5fd9aa0e524ca2d86c9c4
soundestmammal <robert.checco@gmail.com>
2018-02-06 23:50:33 -0500

    Add Wage Function

---
+++ econo/wage.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -
+*-
+
+import numpy as np
+
+def function():
+    year = 0
+    wage = 5.25 + 0.48*(year) - 0.008*(year)**2
+    return wage
+    
+function()
+
+def changeFunction():
+    for x in range(0,100):
+        list[]
+        increase_in_wage = 0.48*(x) - 0.016*(x)
+        
+        
+
+changeFunction()
+
