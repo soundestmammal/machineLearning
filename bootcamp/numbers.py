edb501b3845819c99518c5d6cfd15b9c1203355a
soundestmammal <robert.checco@gmail.com>
2018-02-08 23:33:46 -0500

    Math Fundamentals

---
+++ bootcamp/numbers.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+# There are two types of numbers in Python
+
+#There are Integers and Floating Point numbers
+
+# Integer is a whole number 2 or -2
+# Floating point have a decimal point or can use an E for Power
+
+2+1
+
+2-1
+
+2*2
+
+#Python 2 will truncate the decimal.
+#Python 3 will do true divison
+
+3/2
+
+# Trick if using Python 2
+3.0/2 
+float(3)/2
+
+# Exponential will use double astirisk
+
+2**3 # Equals 2^3 = 8
+
+#Standard order of operations - PEMDAS
+
+#Assigning Labels to Objects
+
+
