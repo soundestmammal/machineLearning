bda3f8dd2293ccb332b67e638882d33daceeae5e
soundestmammal <robert.checco@gmail.com>
2018-02-09 07:31:42 -0500

    Information on Strings

--- bootcamp/numbers.py
+++ bootcamp/numbers.py
@@ -30,4 +30,40 @@ float(3)/2
 
 #Assigning Labels to Objects
 
+a = 5
+
+a + a
+
+a = 12
+a
+a = a + a
+
+a
+
+# 1. Nameds can not start with a number.
+# 2. There can be no spaces in the name, us _ instead.
+# 3. Can't use any of the symbols:'",<>/? etc...
+# 4. It's considered best practice that the names are lowercase.
+
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income * tax_rate
+my_taxes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- bootcamp/strings.py
+++ bootcamp/strings.py
@@ -1,2 +1,9 @@
 # -*- coding: utf-8 -*-
 
+# Strings : Creating a String Used to create text information
+
+"Hello World!"
+
+# Can use both double and single quotes
+
+"I'm a string"
