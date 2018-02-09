7a887276d2b0ccd1522caf04f98861f9d156621f
soundestmammal <robert.checco@gmail.com>
2018-02-09 07:53:38 -0500

    Formatting String Examples

---
+++ bootcamp/format.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+print('This is a string')
+x = 13.13
+
+print("Place my variable here: %s" %(x))
+
+print("Floating point number: %1.3f" %13.145)
+
+print("Convert to string %s" %(123))
+
+print("First: %s, Second %s, Third %s" %("hey", 2, "Seven"))
+print("First: {x}, Second: {y}".format(x="inserted", y="other"))
