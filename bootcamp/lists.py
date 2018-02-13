11e60c5bf274436e0496a1f9d4e4af4802fd5216
soundestmammal <robert.checco@gmail.com>
2018-02-12 22:17:33 -0500

    List and Matrix - nesting

--- bootcamp/lists.py
+++ bootcamp/lists.py
@@ -2,5 +2,38 @@
 
 # Lists general version of sequence
 my_list = [1,2,3]
+# Lists can hold different object types
 new_list = ['string', 23, 1.2, 'o']
 len(my_list)
+
+# Indexing and Slicing
+my_list = ['one', 'two', 'three', 4, 5]
+# my_list[0] returns 'one'
+#my_list[1:] returns 'two' , 'three', 4, 5
+
+my_list[:3]
+
+'hello' + ' world'
+
+# Appends to the end of the list
+# Does not reassign variable. Temporary
+my_list + ['new item']
+
+my_list = my_list + ['new item']
+my_list
+
+# Lists are like arrays, but more flexible
+# They have no type contstraint
+
+
+# Methods
+l = [1,2,3]
+l.append('append me')
+l
+
+l.pop()
+l
+
+x = l.pop(0)
+x
+
--- econo/p_set_one.py
+++ econo/p_set_one.py
@@ -1,2 +1,19 @@
 # -*- coding: utf-8 -*-
 
+# Appendix A Problems 2, 6, 8, 10, 11
+
+# Problem 2: Suppose the following equation describes the relationship between the average number of classes missed during a semester(missed) and the distance from school ( distance, measured in miles):
+import numpy as np
+distance = np.array([5.0, 10.0, 20.0])
+
+def missed(distance):
+        missed = 3 + 0.2*(distance)
+        return missed   
+information = missed(distance)
+
+for info in information:
+    
+    
+         
+    
+
