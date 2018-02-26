64f99c80806b2a62b39c101cd12b2b1f2e93a2a1
soundestmammal <robert.checco@gmail.com>
2018-02-25 23:39:59 -0500

    OOP practice in py

---
+++ bootcamp/oop.py
@@ -0,0 +1,34 @@
+# This is part one of learning about how objects work in Python.
+# Python is a class based language. This is different that other languages such as javascript.
+
+# How to define a class?
+
+class car:
+	pass
+
+car = Vehicle()
+print(car)
+
+# Here car is an object (or instance) of the class Vehicle
+
+#Vehicle class has 4 attributes:
+# Number of wheels
+# Type of tank
+# Seating capacity
+# Maximum Velocity
+
+class Vehicle:
+	def __init__(self, number_of_wheels, type_of_tank, seating_capacity, maximum_velocity):
+		self.number_of_wheels = number_of_wheels
+		self.type_of_tank = type_of_tank
+		self.seating_capacity = seating_capacity
+		self.maximum_velocity = maximum_velocity
+
+#Above we used the init method. We call it a constructor method.
+
+#When we create the vehicle object, we can define this attributes.
+
+# Let's create a tesla model s.
+
+tesla_model_s = Vehicle(4, 'electric', 5, 250)
+
