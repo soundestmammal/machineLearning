67f92cd46d8e8e01b654ba7a3a68c27ea92c47a9
soundestmammal <robert.checco@gmail.com>
2018-03-01 22:35:42 -0500

    Using Polynomial and SVR to forecast salary

---
+++ econo/chebyshev.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+def function(num):
+    ans = 1 - (1/(num^2))
+    print("The percentage is "+ans)
+
+function(1.5)
+
+    
+    
---
+++ ml-az/Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/SVR/svrblank.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+ #  Importing the dataset 
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[: , 1:2].values
+y = dataset.iloc[: , 2].values
+y = y.reshape(-1,1)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
+
+#fitting SVR to the dataset
+from sklearn.svm import SVR
+regressor = SVR(kernel = 'rbf')
+regressor.fit(X, y)
+
+y_pred = regressor.predict(6.5)
+
+# Visualizing the SVR results
+plt.scatter(X, y, color="red")
+plt.plot(X, regressor.predict(X), color='blue')
+plt.title('Truth or Bluff(SVR)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
